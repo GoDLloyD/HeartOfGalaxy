960a96fe50826eb2bfed183145a130ab974db837
GoDLloyD <benjaminmundt1993@gmail.com>
2018-02-18 03:07:58 +0100

    tp after tt

--- HoG/tp_check_files/tp_check.js
+++ HoG/tp_check_files/tp_check.js
@@ -7,7 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	};
 	
 	function update() {
-		document.getElementById("tp_result").innerHTML = "Total TP: " + Math.floor(game.totalTPspent()) + "<br>TP: " + Math.floor(game.techPoints);
+		document.getElementById("tp_result").innerHTML = "Total TP: " + Math.floor(game.totalTPspent()) 
+		+ "<br>TP: " + Math.floor(game.techPoints) 
+		+ "<br>TP after Time Travel: " + Math.floor(game.totalTPspent()+2*game.influence()*Math.log(1+game.totalRPspent()/(200*bi))/Math.log(5));
 	}
 	
 });
