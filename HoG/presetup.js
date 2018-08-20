71a0199ec387b03738cef3e24c83282860e7fa07
GoDLloyD <34571301+GoDLloyD@users.noreply.github.com>
2018-08-20 16:50:12 +0200

    Adjustments to new patch

---
+++ HoG/presetup.js
@@ -0,0 +1,16 @@
+
+var NEW_AUTOROUTES = false;
+var staticBon = 1;
+var dynamicBon = 1;
+var distanceBon = 1;
+var MOBILE_LANDSCAPE = false;
+var MIN_TEXT_SIZE = 0.5;
+var MAX_TEXT_SIZE = 2;
+var DEFAULT_TEXT_SIZE = 1.1;
+
+var staticBon = 1; 
+var dynamicBon = 1; 
+var distanceBon = 1;
+var dynamicTechBon = 1;
+var dynamicResearchBon = 1;
+var bodyTextPct = 90;
